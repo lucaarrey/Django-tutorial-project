2c9cdce13e685f7e48f2363617d81583caf242d7
Luca Bandini <l.bandini@nephila.it>
2017-09-08 18:19:17 +0200

    ended django tutorial part3

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,21 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pubblication_date = models.DateTimeField('published date')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pubblication_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     question_rf = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world, You're at the polls index.")
+    question_list = Question.objects.all().order_by('pubblication_date')
+    #show = ', '.join([q.question_text for q in question_list])
+    context = {'question_list': question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    return HttpResponse("the result of your question: {}".format(question_id))
+
+def vote(request, question_id):
+    return HttpResponse("votes of the question{}".format(question_id))
+
